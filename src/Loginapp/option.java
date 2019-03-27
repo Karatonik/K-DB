bd3340581b4972179d2c1ffa682912d2e9601186
John Doe <mat.kalksztejn@wp.pl>
2019-03-27 19:04:43 +0100

    Create option.java

---
+++ src/Loginapp/option.java
@@ -0,0 +1,14 @@
+package Loginapp;
+
+public enum option {
+    Admin,Student;
+    private option(){}
+
+    public String value(){
+        return  name();
+    }
+    public static  option fromvalue (String v){
+        return valueOf(v);
+    }
+
+}
